04025b4594a42d4d91189ca6fea046476fba442f
Milán Egri <117757061+MilanEgri@users.noreply.github.com>
2023-11-07 14:22:35 +0100

    make basic pages, and frontend routing

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+      Home
+    </div>
+  )
+}
+
+export default Home
