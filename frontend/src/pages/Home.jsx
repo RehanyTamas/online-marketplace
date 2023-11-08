bc17af5051936cf4494fa62a94ae3da02e508eeb
Csaba Katona <115178933+Treev13@users.noreply.github.com>
2023-11-08 13:51:55 +0100

    add css to the item table

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -24,35 +24,42 @@ const Home = () => {
 
   return (
     <div>
-      <div className="ProductsTable">
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Description</th>
-          <th>Price</th>
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        {items.map((item) => (
-          <tr key={item.id}>
-            <td>{item.name}</td>
-            <td>{item.description}</td>
-            <td>{item.price}</td>
-            <td>
-              
-                <button type="button">Update</button>
-              
-              <button type="button" >
-                Delete
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
+
+
+
+      <div className="max-w-screen-md mx-auto bg-white shadow-md p-6 rounded-md">
+        <h2 className="text-2xl font-semibold mb-4">Item List</h2>
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+            <tr>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Item Name</th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Description</th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Price</th>
+              <th>Info</th>
+              <th>Delete</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {items.map((item) => (
+              <tr key={item.id}>
+                <td className="px-6 py-4 whitespace-nowrap">{item.name}</td>
+                <td className="px-6 py-4 whitespace-nowrap">{item.description}</td>
+                <td className="px-6 py-4 whitespace-nowrap">{item.price}</td>
+                <Link to={`/item/${item.id}`}>
+                  <button type="button">Info</button>
+                </Link>
+
+                <td>
+                  <button type="button" >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
