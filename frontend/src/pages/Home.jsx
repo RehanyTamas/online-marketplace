9857bf167f44aacdab2e5259334a5c53a72585e9
Csaba Katona <115178933+Treev13@users.noreply.github.com>
2023-11-08 13:24:29 +0100

    display the items to the Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+
+const fetchItems = () => {
+  return fetch(`http://127.0.0.1:8000/api/items`).then((res) => res.json());
+};
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetchItems()
+      .then((items) => {
+        setLoading(false);
+        setItems(items);
+      });
+  }, []);
+
+  if (loading) {
+    return <h1>Loading</h1>;
+  }
+
   return (
     <div>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-      Home
+      <div className="ProductsTable">
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Price</th>
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((item) => (
+          <tr key={item.id}>
+            <td>{item.name}</td>
+            <td>{item.description}</td>
+            <td>{item.price}</td>
+            <td>
+              
+                <button type="button">Update</button>
+              
+              <button type="button" >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
     </div>
   )
 }
