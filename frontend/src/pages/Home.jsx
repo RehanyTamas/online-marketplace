01b459e931e79299dd347051dff3da329846e610
Csaba Katona <115178933+Treev13@users.noreply.github.com>
2023-11-08 10:38:44 +0100

    add tailwindcss for the project

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,14 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       Home
     </div>
   )
