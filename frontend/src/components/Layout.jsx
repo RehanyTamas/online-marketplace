14aeb55a236422398773f8fad07d529e8b8bf2bd
Csaba Katona <115178933+Treev13@users.noreply.github.com>
2023-11-10 12:05:15 +0100

    format layout

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,17 +4,26 @@ import { Link, Outlet } from 'react-router-dom'
 const Layout = () => {
   return (
     <div>
-      <div>
-      <Link class="no-underline border-b border-blue text-blue-600" to="../register/">
-        register
-      </Link>
-      </div>
-      <div>
-      <Link class="no-underline border-b border-blue text-blue-600" to="../login/">
-        Login
-      </Link>
-      </div>
-      <Outlet />
+      <nav class="flex flex-wrap">
+        <div class="flex-shrink-0 p-2">
+          <Link class="no-underline border-b border-blue text-blue-600" to="../">
+            Home
+          </Link>
+        </div>
+        <div class="flex-shrink-0 p-2">
+          <Link class="no-underline border-b border-blue text-blue-600" to="../register/">
+            Register
+          </Link>
+        </div>
+        <div class="flex-shrink-0 p-2">
+          <Link class="no-underline border-b border-blue text-blue-600" to="../login/">
+            Login
+          </Link>
+        </div>
+      </nav>
+      <section>
+        <Outlet />
+      </section>
     </div>
   )
 }
