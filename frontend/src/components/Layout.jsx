eb31cad443969dddeae918590b9825ffb228f781
Milán Egri <117757061+MilanEgri@users.noreply.github.com>
2023-11-20 13:52:14 +0100

    make not logined navbar

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,26 +1,11 @@
 import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
+import Navbar from './Navbar'
 
 const Layout = () => {
   return (
     <div>
-      <nav class="flex flex-wrap">
-        <div class="flex-shrink-0 p-2">
-          <Link class="no-underline border-b border-blue text-blue-600" to="../">
-            Home
-          </Link>
-        </div>
-        <div class="flex-shrink-0 p-2">
-          <Link class="no-underline border-b border-blue text-blue-600" to="../register/">
-            Register
-          </Link>
-        </div>
-        <div class="flex-shrink-0 p-2">
-          <Link class="no-underline border-b border-blue text-blue-600" to="../login/">
-            Login
-          </Link>
-        </div>
-      </nav>
+      <Navbar />
       <section>
         <Outlet />
       </section>
