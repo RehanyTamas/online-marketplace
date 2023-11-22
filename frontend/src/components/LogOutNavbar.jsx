9b334322f1f4956031e81f47049a1cdc83077ee6
RehanyTamas <Rt19991231>
2023-11-22 15:08:59 +0100

    add alternative handle click

--- frontend/src/components/LogOutNavbar.jsx
+++ frontend/src/components/LogOutNavbar.jsx
@@ -29,6 +29,11 @@ const LogOutNavbar = ({ setIsLogined }) => {
           }).catch(err => console.error(err));
          
     }
+
+
+    function handleClickRedirectOnly(path){
+        navigate(path)
+    }
   return (
     
 
@@ -44,7 +49,15 @@ const LogOutNavbar = ({ setIsLogined }) => {
               </svg>
             </span>
             <span className="text-gray-50">Logout</span>
-          </div>        
+          </div>
+          <div onClick={e => handleClickRedirectOnly('/sales')} className="flex items-center space-x-2 cursor-pointer">
+           <span>
+              <svg className="h-8 w-8 text-yellow-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+              </svg>
+            </span>
+              <span className="text-gray-50">My stuff</span>
+          </div>
         </div>
         <div className="lg:flex hidden items-center space-x-2 bg-white py-1 px-2 rounded-full">
           <span>
