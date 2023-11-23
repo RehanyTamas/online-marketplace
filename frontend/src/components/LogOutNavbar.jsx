4a92f6c1bd0708710cb8ede3837cdf646346013f
Molnár Levente <112870372+molnarlevi19@users.noreply.github.com>
2023-11-23 10:33:24 +0100

    fix home page localStorage clear and add cart button

--- frontend/src/components/LogOutNavbar.jsx
+++ frontend/src/components/LogOutNavbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 
 const LogOutNavbar = ({ setIsLogined }) => {
     let navigate = useNavigate(); 
@@ -35,7 +35,24 @@ const LogOutNavbar = ({ setIsLogined }) => {
   <header>
     <nav className="bg-gray-700">
       <div className="container mx-auto py-4 flex justify-between items-center">
-        <h1 onClick={e => handleClick('/')} className="text-2xl font-bold text-gray-50 cursor-pointer">OnlineMarket</h1>
+          <Link to="/" className="text-2xl font-bold text-gray-50 cursor-pointer">
+              OnlineMarket
+          </Link>
+
+          <div className="flex space-x-10">
+              <div className="flex items-center space-x-2 cursor-pointer">
+                  <Link to="/cart" className="text-2xl font-bold text-gray-50 cursor-pointer">
+                      Cart
+                  </Link>
+                  <span>
+              <svg className="h-8 w-8 text-yellow-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+              </svg>
+            </span>
+                  <span className="text-gray-50"></span>
+              </div>
+          </div>
+
         <div className="flex space-x-10">
           <div onClick={e => handleClick('/login')} className="flex items-center space-x-2 cursor-pointer">
             <span>
@@ -46,6 +63,9 @@ const LogOutNavbar = ({ setIsLogined }) => {
             <span className="text-gray-50">Logout</span>
           </div>        
         </div>
+
+
+
         <div className="lg:flex hidden items-center space-x-2 bg-white py-1 px-2 rounded-full">
           <span>
             <svg  className="h-6 w-6 text-gray-600 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
