ff9a4cddf2e11feabe9181909de1dc6d097a5607
Molnár Levente <112870372+molnarlevi19@users.noreply.github.com>
2023-11-23 10:43:37 +0100

    make the whole cart button clickable

--- frontend/src/components/LogOutNavbar.jsx
+++ frontend/src/components/LogOutNavbar.jsx
@@ -45,10 +45,9 @@ const LogOutNavbar = ({ setIsLogined }) => {
           </Link>
 
           <div className="flex space-x-10">
-              <div className="flex items-center space-x-2 cursor-pointer">
                   <Link to="/cart" className="text-2xl font-bold text-gray-50 cursor-pointer">
+              <div className="flex items-center space-x-2 cursor-pointer">
                       Cart
-                  </Link>
                   <span>
               <svg className="h-8 w-8 text-yellow-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
@@ -56,6 +55,7 @@ const LogOutNavbar = ({ setIsLogined }) => {
             </span>
                   <span className="text-gray-50"></span>
               </div>
+                  </Link>
           </div>
 
         <div className="flex space-x-10">
