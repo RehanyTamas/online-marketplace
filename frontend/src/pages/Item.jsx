6752abaa0aca1c8e55ed590ebb51cfa0d37514fe
Molnár Levente <112870372+molnarlevi19@users.noreply.github.com>
2023-11-24 11:05:19 +0100

    create a connection between cart and add to cart button

--- frontend/src/pages/Item.jsx
+++ frontend/src/pages/Item.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import { Link } from 'react-router-dom';
 
 const fetchItem = (id) => {
@@ -11,7 +11,9 @@ const Item = () => {
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
   const [item, setItem] = useState(null);
-  const [user, setUser] = useState(null);
+  //const [user, setUser] = useState(null);
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
 
   useEffect(() => {
     fetchItem(id)
@@ -25,6 +27,47 @@ const Item = () => {
     return <h1>Loading</h1>;
   }
 
+    const handleAddToCart = (event) => {
+        event.preventDefault();
+        const token = localStorage.getItem("userToken");
+        console.log('Token:', token);
+        console.log('itemId:', item.id);
+
+        if (!token) {
+            return;
+        }
+
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ itemIds: [item.id] }) // Sending item ID directly without an array
+        };
+
+        fetch('http://127.0.0.1:8000/api/purchase', requestOptions)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to add item to the cart');
+                }
+                return response.json();
+            })
+            .then(data => {
+
+                setTimeout(() => {
+                    navigate('/')
+                    window.location.reload();
+                }, 1500);
+            })
+            .catch(error => {
+                setError('Failed to add item to the cart');
+                console.error('Error:', error);
+            });
+    };
+
+
+
   return (
     <div className="max-w-sm mx-auto rounded overflow-hidden shadow-lg">
       <img className="w-full" src="https://via.placeholder.com/350x200" alt="Placeholder" />
@@ -37,8 +80,8 @@ const Item = () => {
       <div className="columns-2 px-6 py-4">
         <div className="font-bold text-orange-500 text-xl text-center mb-2">${item.price}</div>
         <div className="text-center">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Buy It
+          <button onClick={handleAddToCart} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Add to Cart
           </button>
         </div>
       </div>
@@ -53,4 +96,4 @@ const Item = () => {
   )
 }
 
-export default Item
+export default Item;
