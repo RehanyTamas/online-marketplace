463f2ca6014e49460fba8837978da634cdbe4db6
RehanyTamas <Rt19991231>
2024-01-18 09:26:27 +0100

    correct sql file

--- docker/database/database.sql
+++ docker/database/database.sql
@@ -23,18 +23,83 @@ DROP TABLE IF EXISTS `items`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `items` (
-                         `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+                         `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
                          `name` varchar(255) NOT NULL,
-                         `description` text NOT NULL,
-                         `price` decimal(10,2) NOT NULL,
-                         `id_user` int(10) unsigned DEFAULT NULL,
+                         `description` varchar(255) NOT NULL,
+                         `price` double NOT NULL,
+                         `id_user` bigint(20) unsigned DEFAULT NULL,
                          `created_at` timestamp NULL DEFAULT NULL,
                          `updated_at` timestamp NULL DEFAULT NULL,
                          PRIMARY KEY (`id`),
                          KEY `id_user` (`id_user`),
-                         CONSTRAINT `items_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`)
+                         CONSTRAINT `items_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `items`
+--
+
+LOCK TABLES `items` WRITE;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+
+DROP TABLE IF EXISTS `password_reset_tokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `password_reset_tokens` (
+                                         `email` varchar(255) NOT NULL,
+                                         `token` varchar(255) NOT NULL,
+                                         `created_at` timestamp NULL DEFAULT NULL,
+                                         PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `password_reset_tokens`
+--
+
+LOCK TABLES `password_reset_tokens` WRITE;
+/*!40000 ALTER TABLE `password_reset_tokens` DISABLE KEYS */;
+/*!40000 ALTER TABLE `password_reset_tokens` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+DROP TABLE IF EXISTS `personal_access_tokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `personal_access_tokens` (
+                                          `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                                          `tokenable_id` bigint(20) unsigned DEFAULT NULL,
+                                          `tokenable_type` varchar(255) DEFAULT NULL,
+                                          `name` varchar(255) NOT NULL,
+                                          `token` varchar(64) NOT NULL,
+                                          `abilities` text DEFAULT NULL,
+                                          `last_used_at` timestamp NULL DEFAULT NULL,
+                                          `expires_at` timestamp NULL DEFAULT NULL,
+                                          `created_at` timestamp NULL DEFAULT NULL,
+                                          `updated_at` timestamp NULL DEFAULT NULL,
+                                          PRIMARY KEY (`id`),
+                                          UNIQUE KEY `token` (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personal_access_tokens`
+--
+
+LOCK TABLES `personal_access_tokens` WRITE;
+/*!40000 ALTER TABLE `personal_access_tokens` DISABLE KEYS */;
+/*!40000 ALTER TABLE `personal_access_tokens` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `transactions`
@@ -44,21 +109,30 @@ DROP TABLE IF EXISTS `transactions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `transactions` (
-                                `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+                                `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
                                 `name` varchar(255) NOT NULL,
-                                `description` text NOT NULL,
-                                `price` decimal(10,2) NOT NULL,
-                                `id_seller` int(10) unsigned DEFAULT NULL,
-                                `id_buyer` int(10) unsigned DEFAULT NULL,
+                                `description` varchar(255) NOT NULL,
+                                `price` double NOT NULL,
+                                `id_seller` bigint(20) unsigned DEFAULT NULL,
+                                `id_buyer` bigint(20) unsigned DEFAULT NULL,
                                 `created_at` timestamp NULL DEFAULT NULL,
                                 `updated_at` timestamp NULL DEFAULT NULL,
                                 PRIMARY KEY (`id`),
                                 KEY `id_seller` (`id_seller`),
                                 KEY `id_buyer` (`id_buyer`),
-                                CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`id_seller`) REFERENCES `users` (`id`),
-                                CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`id_buyer`) REFERENCES `users` (`id`)
+                                CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`id_seller`) REFERENCES `users` (`id`) ON DELETE SET NULL,
+                                CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`id_buyer`) REFERENCES `users` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transactions`
+--
+
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -68,17 +142,26 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-                         `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-                         `name` varchar(255) NOT NULL,
+                         `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                         `username` varchar(255) NOT NULL,
                          `email` varchar(255) NOT NULL,
-                         `email_verified_at` timestamp NULL DEFAULT NULL,
                          `password` varchar(255) NOT NULL,
                          `remember_token` varchar(100) DEFAULT NULL,
                          `created_at` timestamp NULL DEFAULT NULL,
                          `updated_at` timestamp NULL DEFAULT NULL,
-                         PRIMARY KEY (`id`)
+                         PRIMARY KEY (`id`),
+                         UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -89,4 +172,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-01-17  8:47:39
+-- Dump completed on 2024-01-18  8:23:37
